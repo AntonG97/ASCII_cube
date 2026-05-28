f33aa2cbd28416cf547f17e5ba5913173011eb9b
Anton <anton.grundang@gmail.com>
2026-05-28 06:44:27 +0200

    Updated Shape::Cube class. Structure to begin coding is now at place

--- src/Shape/Shape.h
+++ src/Shape/Shape.h
@@ -33,18 +33,19 @@ class Cube : public Shape, public Rotation
         void rotate() override;
         void project() override;
         void print() override;
+        ~Cube();
 
     private:
         void updateRotationMatrice() override;
-        void drawTriangle(struct vector a, struct vector b, struct vector c, char ascii);
-        void drawFlatTop(struct vector a, struct vector b, struct vector c, char ascii);
-        void drawFlatBot(struct vector a, struct vector b, struct vector c, char ascii);
-        struct vector nearX(struct vector a, struct vector b);
-        struct vector nearY(struct vector a, struct vector b);
+        void drawTriangle(Vector a, Vector b, Vector c, char ascii);
+        void drawFlatTop( Vector a,  Vector b,  Vector c, char ascii);
+        void drawFlatBot( Vector a,  Vector b,  Vector c, char ascii);
+        Vector nearX( Vector a,  Vector b);
+        Vector nearY( Vector a,  Vector b);
         void checkBnd(int *y, int *x);
-        void checkVectorBnd(struct vector *pt);
-        int isFlat(struct vector a, struct vector b, struct vector c);
-        double getSlope(struct vector a, struct vector b);
+        void checkVectorBnd( Vector *pt);
+        int isFlat( Vector a,  Vector b,  Vector c);
+        double getSlope( Vector a,  Vector b);
 
         struct CubeFace
         {
@@ -52,34 +53,8 @@ class Cube : public Shape, public Rotation
             struct Vector *TR;
             struct Vector *BL;
             struct Vector *BR;
-        } faces[6] = {
-            { &_cube_rotated[0], &_cube_rotated[3], &_cube_rotated[4], &_cube_rotated[7] }, // A D E H
-            { &_cube_rotated[1], &_cube_rotated[0], &_cube_rotated[5], &_cube_rotated[4] }, // B A F E
-            { &_cube_rotated[2], &_cube_rotated[1], &_cube_rotated[6], &_cube_rotated[5] }, // C B G F
-            { &_cube_rotated[3], &_cube_rotated[2], &_cube_rotated[7], &_cube_rotated[6] }, // D C H G
-            { &_cube_rotated[1], &_cube_rotated[2], &_cube_rotated[0], &_cube_rotated[3] }, // B C A D
-            { &_cube_rotated[4], &_cube_rotated[7], &_cube_rotated[5], &_cube_rotated[6] }  // E H F G
         };
-
-        const Vector _original_cube[8] = {
-            {  1, -1,  1 },   //A
-            { -1, -1,  1 },   //B
-            { -1,  1,  1 },   //C
-            {  1,  1,  1 },   //D
-            {  1, -1, -1 },   //E
-            { -1, -1, -1 },   //F
-            { -1,  1, -1 },   //G
-            {  1,  1, -1 }    //H 
-        }; 
-        
-        Vector _cube_rotated[8] = {
-            {  1, -1,  1 },   //A
-            { -1, -1,  1 },   //B
-            { -1,  1,  1 },   //C
-            {  1,  1,  1 },   //D
-            {  1, -1, -1 },   //E
-            { -1, -1, -1 },   //F
-            { -1,  1, -1 },   //G
-            {  1,  1, -1 }    //H
-        };
+        const Vector _original_cube[8];
+        Vector _cube_rotated[8];
+        struct CubeFace faces[6];
 };
