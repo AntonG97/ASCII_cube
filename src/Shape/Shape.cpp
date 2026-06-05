e813947d98fa837b4b19867b3d425f06dbfd746c
Anton <anton.grundang@gmail.com>
2026-06-05 08:01:24 +0200

    Updated and added function definitions for the Rotation class

--- src/Shape/Shape.cpp
+++ src/Shape/Shape.cpp
@@ -1,6 +1,67 @@
 #include "Shape.h"
 
-void Rotation::updateRotationMatrice()
+namespace 
 {
-    
+    constexpr double PI_ = 3.14159265358979323846;
+}
+
+Rotation::Angle& Rotation::Angle::operator+=(double rhs) 
+{
+    angle_ = std::fmod(angle_ + rhs, 2 * PI_);
+    return *this;
+}
+
+Rotation::Trig& Rotation::Trig::operator+=(const double rhs)
+{
+    const double angle = rhs;
+    cos_ = std::cos(angle);
+    sin_ = std::sin(angle);
+    return *this;
+}
+// Updates the rotation matrice
+void Rotation::matriceMult()
+{
+    //Get the latest trigonometic values
+    const double cosX = xTrig_.getCos();
+    const double sinX = xTrig_.getSin();
+
+    const double cosY = yTrig_.getCos();
+    const double sinY = yTrig_.getSin();
+
+    const double cosZ = zTrig_.getCos();
+    const double sinZ = zTrig_.getSin();
+
+    //Perform the matrice multiplication
+    //And update the rotation matrice
+    rMatrice_[0][0] = cosZ*cosY;
+	rMatrice_[0][1] = cosZ*sinY*sinX-sinZ*cosX;
+	rMatrice_[0][2] = cosZ*sinY*cosX +sinZ*sinX;
+
+	rMatrice_[1][0] = sinZ*cosY;
+	rMatrice_[1][1] = sinZ*sinY*sinX+cosZ*cosX;
+	rMatrice_[1][2] = sinZ*sinY*cosX-cosZ*sinX;
+
+	rMatrice_[2][0] = -sinY;
+	rMatrice_[2][1] = cosY*sinX;
+	rMatrice_[2][2] = cosY*cosX;
+}
+void Rotation::updateRotationMatrice(
+    const double incrAngleX,
+    const double incrAngleY,
+    const double incrAngleZ
+    )
+{
+    //Update each coordinates trigonometic functions
+    xTrig_ += xAng_.getAngle();
+    yTrig_ += yAng_.getAngle();
+    zTrig_ += zAng_.getAngle();
+
+    //Increment the angles
+    xAng_ += incrAngleX;
+    yAng_ += incrAngleY;
+    zAng_ += incrAngleZ;
+
+    //Perfom the matrice multiplication
+    //Thereby updating the rotation matrice
+    matriceMult();
 }
--- src/Shape/Shape.h
+++ src/Shape/Shape.h
@@ -1,21 +1,70 @@
 #pragma once
 
+#include <cmath>
 #include "Vector.h"
 
 class Rotation
 {
     public:
-        Rotation() : xCoordAng(0), yCoordAng(0), zCoordAng(0), rotationMatrice{} {}
+        Rotation() : 
+        xAng_(0.0), 
+        yAng_(0.0), 
+        zAng_(0.0),
+        xTrig_(xAng_.getAngle()),
+        yTrig_(yAng_.getAngle()),
+        zTrig_(zAng_.getAngle()),
+        rMatrice_{} {}
         void virtual rotate() = 0;
         virtual ~Rotation() = default;
-
+    
+    
     protected:
-        double xCoordAng;
-        double yCoordAng;
-        double zCoordAng;
-        double rotationMatrice[3][3];
+        void updateRotationMatrice(
+            const double incrAngleX = 0.013,
+            const double incrAngleY = 0.025,
+            const double incrAngleZ = 0.026
+        );
 
-        void updateRotationMatrice();
+    private:
+        struct Angle
+        {
+            public:
+                Angle(double ang) : angle_(ang) {}
+                Angle& operator+=(double rhs);
+                double getAngle() const {return angle_;}
+            private:
+                double angle_;
+        };
+
+        struct Trig
+        {
+            public:
+                Trig(double angle) : 
+                    cos_(cos(angle)), 
+                    sin_(sin(angle)) {} 
+                Trig& operator+=(const double rhs);
+                double getCos() const {return cos_;}
+                double getSin() const {return sin_;}
+                private:
+                double cos_;
+                double sin_;
+            };
+            
+            void matriceMult();
+
+            /* Angles defined in radians */
+            Angle xAng_;
+            Angle yAng_;
+            Angle zAng_;
+
+            /* Trigonometic functions */
+            Trig xTrig_;
+            Trig yTrig_;
+            Trig zTrig_;
+
+            /* Rotation matrice */
+            double rMatrice_[3][3];
+
 };
 
 class Shape
