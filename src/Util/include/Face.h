032c1f5bb8b78f49dd75b8dea790d661cf10ce9b
Anton_stationary <anton.grundang@gmail.com>
2026-09-04 21:18:09 +0200

    Added constexpr

--- src/Util/include/Face.h
+++ src/Util/include/Face.h
@@ -8,6 +8,5 @@ struct Face
     const Vec3& c_;
 
     Face(const Vec3& a, const Vec3& b, const Vec3 c) : a_(a), b_(b), c_(c) {}
-    Face(const Face& rhs) = delete;
     Face& operator=(const Face& rhs) = delete;
 };
